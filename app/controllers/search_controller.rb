48766f7dd45bb1ec552e9d6a7eb79c9f2443ba21
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-12 10:26:41 +0530

    feat: render turbo stream

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 class SearchController < ApplicationController
   def search
     @results = Post.search(params[:search])
+    render turbo_stream: turbo_stream.render(
+      'posts',
+      partial: 'posts/posts',
+      locals: { posts: @results }
+    )
   end
 end
