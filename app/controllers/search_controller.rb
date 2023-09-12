372bf6537e4dc3bfeac0641351ed7dcde80f7b54
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-12 10:31:54 +0530

    fix: incorrect method in turbo update

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@
 class SearchController < ApplicationController
   def search
     @results = Post.search(params[:search])
-    render turbo_stream: turbo_stream.render(
+    render turbo_stream: turbo_stream.update(
       'posts',
       partial: 'posts/posts',
       locals: { posts: @results }
